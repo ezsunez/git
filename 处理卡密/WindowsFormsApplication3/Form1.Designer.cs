eef5e8d89368205d8f7ef35c05de77b832e81b8f
ezsunez <82315498@qq.com>
2021-01-13 04:02:00 +0800

    处理卡密2021.1.13

---
+++ 处理卡密/WindowsFormsApplication3/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace WindowsFormsApplication3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(437, 250);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(509, 140);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "转换";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "20.00",
+            "30.00",
+            "50.00",
+            "100.00"});
+            this.comboBox1.Location = new System.Drawing.Point(509, 95);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(659, 280);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ 处理卡密/WindowsFormsApplication3/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string input = richTextBox1.Text.Trim();
+            string pattern = @"[0-9]{19}";
+            Regex regex = new Regex(pattern);
+            var arr = regex.Matches(input).Cast<Match>().ToArray();
+            string amount = "";
+            if (comboBox1.Text == "100.00") amount = "10.00";
+            else amount = comboBox1.Text;
+            string sn1 = amount.Substring(0, amount.IndexOf(".00")) + DateTime.Now.Year.ToString().Substring(2, 2) + DateTime.Now.ToString("MMddHHmm");
+            richTextBox1.Clear();
+            int sn2 = 0;
+            string sn = "";
+            foreach (var i in arr)
+            {
+                sn2++;
+                 if (sn2 > 99)
+                { sn = sn1 + sn2.ToString(); }
+                else if(sn2 < 10)
+                { sn=sn1 + "00" + sn2.ToString(); }
+                else if (sn2>9)
+                    { sn = sn1 +"0"+ sn2.ToString(); }
+                
+                richTextBox1.Text += sn + "\t" + i.Value.Trim() + "\t"+ comboBox1.Text + Environment.NewLine;
+                     }
+            string dk = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+            richTextBox1.SaveFile(dk + "\\130"+comboBox1.Text.Substring(0, comboBox1.Text.IndexOf(".00"))+"mini-"+sn2.ToString()+"-"+ DateTime.Now.ToString("yyyy年MM月dd日HH点mm分ss")+".txt", RichTextBoxStreamType.PlainText);
+        }
+    }
+}
