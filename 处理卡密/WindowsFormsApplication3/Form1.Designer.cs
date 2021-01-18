6b41b0263000808642ee621ebf65d1479ea10c45
ezsunez <82315498@qq.com>
2021-01-19 03:59:32 +0800

    20210119

--- 处理卡密/WindowsFormsApplication3/Form1.Designer.cs
+++ 处理卡密/WindowsFormsApplication3/Form1.Designer.cs
@@ -30,11 +30,11 @@
         {
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.button1 = new System.Windows.Forms.Button();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // richTextBox1
             // 
+            this.richTextBox1.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.richTextBox1.Location = new System.Drawing.Point(12, 12);
             this.richTextBox1.Name = "richTextBox1";
             this.richTextBox1.Size = new System.Drawing.Size(437, 250);
@@ -43,7 +43,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(509, 140);
+            this.button1.Location = new System.Drawing.Point(21, 268);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
@@ -51,25 +51,11 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // comboBox1
-            // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
-            "20.00",
-            "30.00",
-            "50.00",
-            "100.00"});
-            this.comboBox1.Location = new System.Drawing.Point(509, 95);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 20);
-            this.comboBox1.TabIndex = 2;
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(659, 280);
-            this.Controls.Add(this.comboBox1);
+            this.ClientSize = new System.Drawing.Size(489, 304);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.richTextBox1);
             this.Name = "Form1";
@@ -82,7 +68,6 @@
 
         private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
 
--- 处理卡密/WindowsFormsApplication3/Form1.cs
+++ 处理卡密/WindowsFormsApplication3/Form1.cs
@@ -21,30 +21,51 @@ namespace WindowsFormsApplication3
         private void button1_Click(object sender, EventArgs e)
         {
             string input = richTextBox1.Text.Trim();
-            string pattern = @"[0-9]{19}";
+            //string pattern = @"[0-9]{19}";
+            string pattern = @"(?<=张)\d{2,3}(?=元充值卡)|[0-9]{19}";
             Regex regex = new Regex(pattern);
             var arr = regex.Matches(input).Cast<Match>().ToArray();
-            string amount = "";
-            if (comboBox1.Text == "100.00") amount = "10.00";
-            else amount = comboBox1.Text;
-            string sn1 = amount.Substring(0, amount.IndexOf(".00")) + DateTime.Now.Year.ToString().Substring(2, 2) + DateTime.Now.ToString("MMddHHmm");
             richTextBox1.Clear();
-            int sn2 = 0;
-            string sn = "";
+
+            string sn2 = DateTime.Now.Year.ToString().Substring(2, 2) + DateTime.Now.ToString("MMddHHmm");
+            Dictionary<string, List<string>> dict1 = new Dictionary<string, List<string>>();
+            var lst = new List<string>();
+            string key = "";
             foreach (var i in arr)
             {
-                sn2++;
-                 if (sn2 > 99)
-                { sn = sn1 + sn2.ToString(); }
-                else if(sn2 < 10)
-                { sn=sn1 + "00" + sn2.ToString(); }
-                else if (sn2>9)
-                    { sn = sn1 +"0"+ sn2.ToString(); }
-                
-                richTextBox1.Text += sn + "\t" + i.Value.Trim() + "\t"+ comboBox1.Text + Environment.NewLine;
+                if (i.Value.Length < 4)
+                {
+                    
+                    if (!dict1.ContainsKey(i.Value))
+                    {
+                        dict1.Add(i.Value, new List<string>());
+                    }
+                    key = i.Value;
+                    lst = dict1[i.Value];
+                }
+                else
+                {
+                    lst.Add(i.Value);           
+                }
                      }
-            string dk = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
-            richTextBox1.SaveFile(dk + "\\130"+comboBox1.Text.Substring(0, comboBox1.Text.IndexOf(".00"))+"mini-"+sn2.ToString()+"-"+ DateTime.Now.ToString("yyyy年MM月dd日HH点mm分ss")+".txt", RichTextBoxStreamType.PlainText);
+            foreach (var j in dict1.Keys)
+            {
+                richTextBox1.Clear();
+                lst = dict1[j];
+               string sn = j.Substring(0, 2) + sn2 + "001";
+                long a = long.Parse(sn);
+                int i = 0;
+                foreach (string rows in lst)
+                {
+                    i++;
+                    sn=a.ToString();
+                    a++;
+                    richTextBox1.Text += sn + "\t" + rows + "\t" + j + ".00" + Environment.NewLine;
+                }
+                string dk = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+                richTextBox1.SaveFile(dk + "\\130" + j + "mini-" + i.ToString() + "-" + DateTime.Now.ToString("yyyy年MM月dd日HH点mm分ss") + ".txt", RichTextBoxStreamType.PlainText);
+            }
+
         }
     }
 }
