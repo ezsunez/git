05fa6a5ca0b975faa428ea262f80232bca554abf
ezsunez <82315498@qq.com>
2021-02-02 23:13:17 +0800

    2021-02-02

---
+++ python test/LIS 连续递增序列.py
@@ -0,0 +1,14 @@
+
+#  Longest increasing subsequence
+
+def lee(A):
+    W = len(A[0])
+    dp= [1 for _ in range(len(A[0]))]
+    for i in range(W-2,-1,-1):
+        for j in range(i+1,W):
+            if all(r[i]<r[j] for r in A):
+                dp[i]=max(dp[i],1+dp[j])
+    return W-max(dp)
+
+
+print (lee(["abdc"]))
